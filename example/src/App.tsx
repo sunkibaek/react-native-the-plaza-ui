939603379d087850c8657052e3fdfc6d27716892
Sunki Baek <sunki.baek@gmail.com>
2020-12-11 21:05:46 -0800

    chore: edit to use default prettier rules

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import ThePlazaUi from 'react-native-the-plaza-ui';
+import * as React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import ThePlazaUi from "react-native-the-plaza-ui";
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
@@ -19,7 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
